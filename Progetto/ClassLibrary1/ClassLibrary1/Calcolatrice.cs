c5bdcf4344bd30b4de923ee339d148a6107f51d9
FeeF <Francesco.migliardi.14-2@accademiadigitaleliguria.it>
2025-06-25 15:37:09 +0200

    Modifica calcolatrice

---
+++ Progetto/ClassLibrary1/ClassLibrary1/Calcolatrice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcolatrice
+{
+    public class Calcolatrice
+    {
+        public Calcolatrice() { }
+        public double Somma(double a, double b)
+        {
+            return 5;
+            //throw new NotImplementedException();
+
+        }
+    }
+
+}
---
+++ Progetto/ClassLibrary1/TestCalcolatrice/Test1.cs
@@ -0,0 +1,15 @@
+﻿namespace TestCalcolatrice
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSomma()
+        {
+            Calcolatrice.Calcolatrice calcolatrice = new Calcolatrice.Calcolatrice();
+            double risultato = calcolatrice.Somma(2, 3);
+            Assert.AreEqual(5, risultato);
+
+        }
+    }
+}
